837e4e835bad365c2509a9ad45e730b98a1851db
RajuManur143 <rajumannur18@gmail.com>
2026-07-17 10:13:34 +0530

    "IIFE METHOD ADDED"

---
+++ DAY7/IMMEDIATEINVOKEFUNCTION/area.js
@@ -0,0 +1,11 @@
+// Syntax of immediate ivoke function (function f_name{})   //invoke()
+// Syntax () ()
+(function area_of_circle() {
+    let r = 10;
+    const PI = 3.142;
+    let res = PI * r * r;
+    console.log(res);
+})
+
+// invoke
+();
